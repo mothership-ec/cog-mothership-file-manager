1dcb2d6edb891613d918bb8aa29a2396934d52f1
Ewan Valentine <ewan@message.co.uk>
2013-05-29 10:13:32 +0100

    Doc blocks

--- src/Message/Mothership/FileManager/File/Delete.php
+++ src/Message/Mothership/FileManager/File/Delete.php
@@ -9,27 +9,51 @@ use Message\Cog\DB\Query;
 	 * Author Ewan Valentine <ewan@message.co.uk> 
 	 * Copyright Message 2013
 	 *
-	 * @return $this->_file 
-	 * 
+	 * Basic file deletion class for database records
+	 * Saves who deleted a file and when.
+	 *
+	 * @todo remove dummy user
 	 */
 class Delete
 {
 
+	/**
+	 * @access protected 
+	 */
 	protected $_file;
+
+	/**
+	 * @access protected 
+	 */
 	protected $_query;
 
+	/**
+	 * @access public
+	 * @param File $file Query $query
+	 */
 	public function __construct(File $file, Query $query)
 	{
 		$this->_file = $file;
 		$this->_query = $query;
 	}
 
+	/**
+	 * Sets file in database marked as deleted.
+	 * Keeps a record of who a file was deleted by and when.
+	 *
+	 * @return $this->_file
+	 * @access public
+	 */
 	public function delete()
 	{
 
+		/** new Datatime */
 		$date = new \Datetime;
+
+		/** Dummy user */
 		$user = 1;
 
+		/** Query to set deletion info */
 		$result = $this->_query->run("
 			UPDATE
 				file
@@ -48,7 +72,10 @@ class Delete
 				$this->_file->id
 			));
 
+		/** Returns deletion date */
 		$this->_file->deletedAt = $date;
+
+		/** Returns deleted by */
 		$this->_file->deletedBy = $user;
 
 		return $this->_file;
