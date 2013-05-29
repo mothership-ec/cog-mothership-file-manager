8532d7daaa86d84eb279da97a1e2d47513a0a904
Danny Hannah <dannyhannah87@gmail.com>
2013-05-29 11:07:11 +0100

    Adding in event, a fix or two and adding some code

--- src/Message/Mothership/FileManager/File/Edit.php
+++ src/Message/Mothership/FileManager/File/Edit.php
@@ -2,16 +2,90 @@
 
 namespace Message\Mothership\FileManager\File;
 
+use Message\Mothership\FileManager\Event\FileEvent;
+
+use Message\Cog\Event\DispatcherInterface;
+use Message\Cog\DB\Query as DBQuery;
+
 class Edit
 {
 
-	public function __construct()
-	{
+	protected $_query;
+	protected $_eventDispatcher;
 
+	public function __construct(DBQuery $query, DispatcherInterface $eventDispatcher)
+	{
+		$this->_query = $query;
+		$this->_eventDispatcher = $eventDispatcher;
 	}
 
-	public function save()
+/**
+ * Update changes to a file object into the DB. Method also fires FileEvent
+ *
+ * @todo make $userID an instance of User; $data could also be cloned instance of File too
+ * which would make it easier when passing in the data?
+ *
+ * @param  File   $file 	The $file object that needs to be updated
+ * @param  [type] $data 	An array of changes that have been made
+ * @param  [type] $userID 	The userId who made the change
+ *
+ * @return [type]
+ */
+	public function save(File $file, $data, $userID = 1)
 	{
+		// Update any properties that have changed and update them on the
+		// $file object
+		foreach ($data as $key => $value) {
+			if (isset($file->{$key}) && $file->{$key} != $value) {
+				$file->{$key} = $value;
+			}
+		}
+
+		$result = $this->_query->run('
+			UPDATE
+				file
+			SET
+				url = ?s,
+				name = ?s,
+				extension = ?s,
+				file_size = ?s,
+				updated_at = UNIX_TIMESTAMP(),
+				updated_by = ?i,
+				type_id = ?i,
+				checksum = ?s,
+				preview_url = ?s,
+				dimension_x = ?i,
+				dimension_y = ?i,
+				alt_text = ?s,
+				duration = ?i
+			WHERE
+				file_id = ?i
+		', array(
+			$file->url,
+			$file->name,
+			$file->extension,
+			$file->fileSize,
+			$userID,
+			$file->typeID,
+			$file->checksum,
+			$file->previewUrl,
+			$file->dimensionX,
+			$file->dimensionY,
+			$file->altText,
+			$file->duration,
+			$file->fileID,
+		));
+
+		// Initiate the event file
+		$event = new FileEvent($file);
+
+		// Dispatch the file created event
+		$this->_eventDispatcher->dispatch(
+			FileEvent::EDIT,
+			$event
+		);
+
+		return $result->affected() ? $file : false;
 
 	}
 }
