9c5328aabb2222a03d3a158c54894afe95cb5d25
Danny Hannah <dannyhannah87@gmail.com>
2013-06-05 16:08:51 +0100

    Adding in delete stuff although it hasn't been tested as awaiting a QA on the delete stuff for Ewan.
    Also changed the functionality a bit for the edit stuff to make sure that the changes happen within the if statement which checks for the post data

--- src/Message/Mothership/FileManager/Controller/Detail.php
+++ src/Message/Mothership/FileManager/Controller/Detail.php
@@ -23,17 +23,29 @@ class Detail extends \Message\Cog\Controller\Controller
 
 	public function edit($fileID)
 	{
-		// Load the changed data from the request
-		$edits = $this->_services['request']->get('file');
 		// Load the file object
 		$file = $this->_services['filesystem.file.loader']->getByID($fileID);
-		// Set the alt text
-		$file->altText = $edits['alt_text'];
-		// Turn the tags into an array and trim the values
-		$file->tags = array_filter(array_map('trim', explode(',',$edits['tags'])));
-		// Save the file
-		$file = $this->_services['filesystem.file.edit']->save($file);
+		// Load the changed data from the request
+		if ($edits = $this->_services['request']->get('file')) {
+			// Set the alt text
+			$file->altText = $edits['alt_text'];
+			// Turn the tags into an array and trim the values
+			$file->tags = array_filter(array_map('trim', explode(',',$edits['tags'])));
+			// Save the file
+			$file = $this->_services['filesystem.file.edit']->save($file);
+		}
 		// Redriect the page to where is was
 		return $this->redirect($this->generateUrl('filemanager.detail',array('fileID' => $file->fileID)));
+	}
+
+	public function delete($fileID)
+	{
+		// Load the file object
+		$file = $this->_services['filesystem.file.loader']->getByID($fileID);
+		// Check that the delete request has been sent
+		if ($delete = $this->_services['request']->get('delete')) {
+			$file = $this->_services['filesystem.file.delete']->delete($file);
+		}
+		return $this->redirect($this->generateUrl('filemanager.listing',array('fileID' => $file->fileID)));
 	}
 }
