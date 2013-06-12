207b6541f4c7d4dc2b06898cd47ab60618029ec4
James Moss <james@message.co.uk>
2013-06-12 10:45:10 +0100

    Updating _services to get()

--- src/Message/Mothership/FileManager/Controller/Detail.php
+++ src/Message/Mothership/FileManager/Controller/Detail.php
@@ -16,8 +16,8 @@ class Detail extends \Message\Cog\Controller\Controller
 	 */
 	public function index($fileID)
 	{
-		$file = $this->_services['filesystem.file.loader']->getByID($fileID);
-		$author = $this->_services['user.loader']->getByID($file->authorship->createdBy());
+		$file = $this->get('filesystem.file.loader')->getByID($fileID);
+		$author = $this->get('user.loader')->getByID($file->authorship->createdBy());
 		$data = array(
 			'file' => $file,
 			'author' => $author,
@@ -33,15 +33,15 @@ class Detail extends \Message\Cog\Controller\Controller
 	public function edit($fileID)
 	{
 		// Load the file object
-		$file = $this->_services['filesystem.file.loader']->getByID($fileID);
+		$file = $this->get('filesystem.file.loader')->getByID($fileID);
 		// Load the changed data from the request
-		if ($edits = $this->_services['request']->get('file')) {
+		if ($edits = $this->get('request')->get('file')) {
 			// Set the alt text
 			$file->altText = $edits['alt_text'];
 			// Turn the tags into an array and trim the values
 			$file->tags = array_filter(array_map('trim', explode(',',$edits['tags'])));
 			// Save the file
-			if ($file = $this->_services['filesystem.file.edit']->save($file)) {
+			if ($file = $this->get('filesystem.file.edit')->save($file)) {
 				$this->addFlash('success', $file->file->getBasename().' was updated successfully');
 			} else {
 				$this->addFlash('error', $file->file->getBasename().' could not be updated.');
@@ -59,11 +59,11 @@ class Detail extends \Message\Cog\Controller\Controller
 	public function delete($fileID)
 	{
 		// Check that the delete request has been sent
-		if ($delete = $this->_services['request']->get('delete')) {
+		if ($delete = $this->get('request')->get('delete')) {
 			// Load the file object
-			$file = $this->_services['filesystem.file.loader']->getByID($fileID);
+			$file = $this->get('filesystem.file.loader')->getByID($fileID);
 
-			if ($file = $this->_services['filesystem.file.delete']->delete($file)) {
+			if ($file = $this->get('filesystem.file.delete')->delete($file)) {
 				$this->addFlash('success', $file->file->getBasename().' was deleted. <a href="'.$this->generateUrl('ms.cp.file_manager.restore',array('fileID' => $file->id)).'">Undo</a>');
 			} else {
 				$this->addFlash('error', $file->file->getBasename().' could not be deleted.');
@@ -82,9 +82,9 @@ class Detail extends \Message\Cog\Controller\Controller
 	public function restore($fileID)
 	{
 		// Load the file
-		$file = $this->_services['filesystem.file.loader']->includeDeleted(true)->getByID($fileID);
+		$file = $this->get('filesystem.file.loader')->includeDeleted(true)->getByID($fileID);
 
-		if ($this->_services['filesystem.file.delete']->restore($file)) {
+		if ($this->get('filesystem.file.delete')->restore($file)) {
 			$this->addFlash('success', $file->file->getBasename().' was restored successfully');
 		} else {
 			$this->addFlash('error', $file->file->getBasename().' could not be restored.');
