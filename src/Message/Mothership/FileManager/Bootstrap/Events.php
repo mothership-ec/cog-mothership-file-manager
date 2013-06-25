2455f8398366f6268cc5cb3347b316761c06c720
Joe Holdcroft <joe@message.co.uk>
2013-06-25 17:06:35 +0100

    Updating menu registration logic and renaming route collection. Closes #14

--- src/Message/Mothership/FileManager/Bootstrap/Events.php
+++ src/Message/Mothership/FileManager/Bootstrap/Events.php
@@ -12,14 +12,7 @@ class Events implements EventsInterface
 	{
 		$dispatcher->addListener(Event::BUILD_MAIN_MENU, function($event) {
 			$event->addItem('ms.cp.file_manager.listing', 'File Manager', array(
-				'ms.cp.file_manager.listing',
-				'ms.cp.file_manager.search.forward',
-				'ms.cp.file_manager.search',
-				'ms.cp.file_manager.upload',
-				'ms.cp.file_manager.detail',
-				'ms.cp.file_manager.edit',
-				'ms.cp.file_manager.delete',
-				'ms.cp.file_manager.restore',
+				'ms.cp.file_manager'
 			));
 		});
 	}
--- src/Message/Mothership/FileManager/Bootstrap/Routes.php
+++ src/Message/Mothership/FileManager/Bootstrap/Routes.php
@@ -8,31 +8,31 @@ class Routes implements RoutesInterface
 {
 	public function registerRoutes($router)
 	{
-		$router['files']->setPrefix('/files')->setParent('ms.cp');
+		$router['ms.cp.file_manager']->setPrefix('/files')->setParent('ms.cp');
 
-		$router['files']->add('ms.cp.file_manager.listing', '/', '::Controller:Listing#index')
+		$router['ms.cp.file_manager']->add('ms.cp.file_manager.listing', '/', '::Controller:Listing#index')
 			->setMethod('GET');
 
-		$router['files']->add('ms.cp.file_manager.search.forward', '/search', '::Controller:Listing#searchRedirect')
+		$router['ms.cp.file_manager']->add('ms.cp.file_manager.search.forward', '/search', '::Controller:Listing#searchRedirect')
 			->setMethod('POST');
 
-		$router['files']->add('ms.cp.file_manager.search', '/search/{term}', '::Controller:Listing#search');
+		$router['ms.cp.file_manager']->add('ms.cp.file_manager.search', '/search/{term}', '::Controller:Listing#search');
 
-		$router['files']->add('ms.cp.file_manager.upload', '/upload', '::Controller:Upload#index')
+		$router['ms.cp.file_manager']->add('ms.cp.file_manager.upload', '/upload', '::Controller:Upload#index')
 			->setMethod('POST');
 
-		$router['files']->add('ms.cp.file_manager.detail', '/{fileID}', '::Controller:Detail#index')
+		$router['ms.cp.file_manager']->add('ms.cp.file_manager.detail', '/{fileID}', '::Controller:Detail#index')
 			->setRequirement('fileID', '\d+');
 
-		$router['files']->add('ms.cp.file_manager.edit', '/{fileID}/edit', '::Controller:Detail#edit')
+		$router['ms.cp.file_manager']->add('ms.cp.file_manager.edit', '/{fileID}/edit', '::Controller:Detail#edit')
 			->setRequirement('fileID', '\d+')
 			->setMethod('POST');
 
-		$router['files']->add('ms.cp.file_manager.delete', '/{fileID}/delete', '::Controller:Detail#delete')
+		$router['ms.cp.file_manager']->add('ms.cp.file_manager.delete', '/{fileID}/delete', '::Controller:Detail#delete')
 			->setRequirement('fileID', '\d+')
 			->setMethod('DELETE');
 
-		$router['files']->add('ms.cp.file_manager.restore', '/{fileID}/restore/{hash}', '::Controller:Detail#restore')
+		$router['ms.cp.file_manager']->add('ms.cp.file_manager.restore', '/{fileID}/restore/{hash}', '::Controller:Detail#restore')
 			->setRequirement('fileID', '\d+')
 			->setMethod('GET')
 			->enableCsrf('hash');
