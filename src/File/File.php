3543ae7fd3d85bf95ef38284e84d67229b53e3c1
Joe Holdcroft <joe@message.co.uk>
2014-05-19 11:21:53 +0100

    Allow `File` to be serialized safely

--- src/File/File.php
+++ src/File/File.php
@@ -4,6 +4,7 @@ namespace Message\Mothership\FileManager\File;
 
 use Message\ImageResize\ResizableInterface;
 
+use Message\Cog\Filesystem\File as FileSystemFile;
 use Message\Cog\ValueObject\Authorship;
 
 /**
@@ -30,9 +31,49 @@ class File implements ResizableInterface
 
 	public $file;
 
+	protected $_filePath;
+
+	/**
+	 * Constructor.
+	 */
 	public function __construct()
 	{
 		$this->authorship = new Authorship;
+	}
+
+	/**
+	 * Magic sleep method. Called before this object is serialized.
+	 *
+	 * Here, we drop the instance of `Filesystem\File` in Cog because it extends
+	 * `SplFileInfo` so it cannot be serialized. We save the file path so we can
+	 * re-instantiate it on `__wakeup()`.
+	 *
+	 * @return array Property names to include in serialization (all except for
+	 *               `file`)
+	 */
+	public function __sleep()
+	{
+		// Save the file path
+		$this->_filePath = $this->file->getRealPath();
+
+		// Get all properties for this object
+		$vars = get_object_vars($this);
+
+		// Remove the `file` property
+		unset($vars['file']);
+
+		return array_keys($vars);
+	}
+
+	/**
+	 * Magic wake up method. Called after this object is unserialized.
+	 *
+	 * Here, we rebuild the instance of `Filesystem\File` in Cog from the saved
+	 * file path.
+	 */
+	public function __wakeup()
+	{
+		$this->file = new FileSystemFile($this->_filePath);
 	}
 
 	public function getUrl()
